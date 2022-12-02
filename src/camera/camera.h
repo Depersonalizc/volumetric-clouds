345033f4c6bfb159a3b9fe622745a8b822cee9fe
Depersonalizc <1046770165@qq.com>
2022-12-02 15:42:41 -0500

    default camera position

--- src/camera/camera.h
+++ src/camera/camera.h
@@ -8,8 +8,10 @@
 
 
 struct SceneCameraData {
-    glm::vec4 pos  = {0.0, 0.0, 3.0, 1.0};
-    glm::vec4 look = {0.0, 0.0, -1.0, 0.0};
+//    glm::vec4 pos  = {0.0, 0.0, 3.0, 1.0};
+//    glm::vec4 look = {0.0, 0.0, -1.0, 0.0};
+    glm::vec4 pos  = {0.0, -10.0, 0.0, 1.0};
+    glm::vec4 look = {0.0, 1.0, -0.0001, 0.0};
     glm::vec4 up   = {0.0, 1.0, 0.0, 0.0};
 
     double heightAngle = glm::quarter_pi<double>();
