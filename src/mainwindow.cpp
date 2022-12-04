894a19e572868d75166c78cffd37be2916293a98
Viviana Wei <viviana_wei@brown.edu>
2022-12-04 15:18:49 -0500

    feat: window supports resizing

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -15,8 +15,6 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
-
 void MainWindow::on_densityMultiplier_valueChanged(double arg1)
 {
     ui->densityMultiplier->setValue(arg1);
@@ -24,13 +22,57 @@ void MainWindow::on_densityMultiplier_valueChanged(double arg1)
     realtime->settingsChanged();
 }
 
-
-
-
 void MainWindow::on_stepNumber_spinBox_valueChanged(int arg1)
 {
     ui->stepNumber_spinBox->setValue(arg1);
     settings.numSteps = ui->stepNumber_spinBox->value();
+    realtime->settingsChanged();
+}
+
+void MainWindow::on_stepSize_spinBox_valueChanged(int arg1)
+{
+    ui->stepSize_spinBox->setValue(arg1);
+    settings.stepSize = ui->stepSize_spinBox->value();
+    realtime->settingsChanged();
+}
+
+
+void MainWindow::on_lightAborspMulti_doubleSpinBox_valueChanged(double arg1)
+{
+    ui->lightAborspMulti_doubleSpinBox->setValue(arg1);
+   // settings. = ui->stepSize_spinBox->value();
+    realtime->settingsChanged();
+}
+
+
+void MainWindow::on_lightTrans_doubleSpinBox_valueChanged(double arg1)
+{
+    ui->lightTrans_doubleSpinBox->setValue(arg1);
+   // settings.
+    realtime->settingsChanged();
+}
+
+
+void MainWindow::on_valScaling1_doubleSpinBox_valueChanged(double arg1)
+{
+    ui->valScaling1_doubleSpinBox->setValue(arg1);
+    settings.volumeScaling[1] = ui->valScaling1_doubleSpinBox->value();
+    realtime->settingsChanged();
+}
+
+
+void MainWindow::on_valScaling2_doubleSpinBox_valueChanged(double arg1)
+{
+    ui->valScaling2_doubleSpinBox->setValue(arg1);
+    settings.volumeScaling[2] = ui->valScaling2_doubleSpinBox->value();
+    realtime->settingsChanged();
+}
+
+
+void MainWindow::on_valScaling3_doubleSpinBox_valueChanged(double arg1)
+{
+    ui->valScaling3_doubleSpinBox->setValue(arg1);
+    settings.volumeScaling[3] = ui->valScaling3_doubleSpinBox->value();
     realtime->settingsChanged();
 }
 
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -22,6 +22,18 @@ private slots:
 
     void on_stepNumber_spinBox_valueChanged(int arg1);
 
+    void on_stepSize_spinBox_valueChanged(int arg1);
+
+    void on_lightAborspMulti_doubleSpinBox_valueChanged(double arg1);
+
+    void on_lightTrans_doubleSpinBox_valueChanged(double arg1);
+
+    void on_valScaling1_doubleSpinBox_valueChanged(double arg1);
+
+    void on_valScaling2_doubleSpinBox_valueChanged(double arg1);
+
+    void on_valScaling3_doubleSpinBox_valueChanged(double arg1);
+
 private:
     Ui::MainWindow *ui;
     Realtime *realtime;
